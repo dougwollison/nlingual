044da04e52c51096d568ab7d683bde3485619730
Doug Wollison <doug@ninesides.com>
2013-09-13 16:15:54 -0400

    Reworked main plugin file

--- nLingual.php
+++ nLingual.php
@@ -9,17 +9,4 @@ Author URI: http://dougw.me
 License: GPL2
 */
 
-require 'nLingual.class.php';
-require 'nLingual.aliases.php';
-
-//Initial nLingual
-nLingual::init();
-
-require 'local.php';
-require 'utilities.php';
-require 'hooks.php';
-
-if(is_admin()){
-	require 'admin.hooks.php';
-	require 'admin.settings.php';
-}
+require('php/load.php');
