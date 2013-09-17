bbd3a08873e3f5784620f52b6d1e1f9e86e629e8
Doug Wollison <doug@ninesides.com>
2013-09-17 17:25:04 -0400

    Modified hooks and meta boxes for using nL_translations, and update metabox names.

--- php/admin.hooks.php
+++ php/admin.hooks.php
@@ -6,14 +6,14 @@ add_action('add_meta_boxes', 'nLingual_add_meta_box');
 function nLingual_add_meta_box(){
 	foreach(nL_post_types() as $type){
 		add_meta_box(
-			'language',
-			'Language',
+			'nLingual_language',
+			'nLingual: '.ucwords(str_replace('_', '', $type)).' Language',
 			'nLingual_language_metabox',
 			$type
 		);
 		add_meta_box(
-			'translations',
-			'Translations',
+			'nLingual_translations',
+			'nLingual: Translated Versions',
 			'nLingual_translations_metabox',
 			$type
 		);
@@ -23,8 +23,8 @@ function nLingual_add_meta_box(){
 function nLingual_language_metabox($post){
 	?>
 	<select name="language" style="width:100%">
-	<?php foreach(nL_languages() as $slug => $lang): $lang = get_term_by('slug', $slug, 'language');?>
-		<option value="<?php echo $lang->term_id?>" <?php if(nL_in_this_lang($post->ID, $slug)) echo 'selected'?>><?php echo $lang->name?></option>
+	<?php foreach(nL_languages() as $slug => $lang):?>
+		<option value="<?php echo $slug?>" <?php if(nL_in_this_lang($post->ID, $slug)) echo 'selected'?>><?php echo $lang['name']?></option>
 	<?php endforeach;?>
 	</select>
 	<?php
@@ -34,70 +34,31 @@ function nLingual_translations_metabox($post){
 	global $wpdb;
 	wp_nonce_field(__FILE__, 'nLingual_translations');
 
-	if(nL_in_default_lang($post)){
-		// If in the default language (the original) offer a list of posts in each language to select as the translated version
-		foreach(nL_languages() as $lang => $data){
-			if($lang == nL_default_lang()) continue;
+	// Loop through each language and present controls for each translation
+	foreach(nL_languages() as $lang => $data){
+		if($lang == nL_get_post_lang()) continue;
 
-			$tp = get_post_meta($post->ID, "_translated_$lang", true);
+		$translation = nL_get_translation($post->ID, $lang, false);
 
-			// Get a list of available posts in the selected language
-			$lang_posts = get_posts(array(
-				'post_type' => $post->post_type,
-				'post_status' => 'publish',
-				'posts_per_page' => -1,
-				'language' => $lang,
-				'orderby' => 'post_title',
-				'order' => 'ASC',
-			));
-
-			?>
-			<p>
-				<?php if($tp > 0):?>
-				<a href="<?php echo admin_url('/post.php')?>?post_type=<?php echo get_post_type($tp)?>&post=<?php echo $tp?>&action=edit" class="button" target="_blank">Edit "<?php echo $wpdb->get_var("SELECT post_title FROM $wpdb->posts WHERE ID = $tp")?>"</a>
-				Or...
-				<?php endif;?>
-			</p>
-			<p><label>
-
-				Assign <?php echo $data['name']?> post:
-				<select name="translations[<?php echo $lang?>]">
-					<option value="-1">Select <?php echo $post->post_type?></option>
-				<?php foreach($lang_posts as $lang_post): ?>
-					<option value="<?php echo $lang_post->ID?>" <?php if($lang_post->ID == $tp) echo 'selected'?>><?php echo $lang_post->post_title?></option>
-				<?php endforeach;?>
-				</select>
-			</label></p>
-			<p>Or... <a href="<?php echo admin_url()?>?nL_new_translation=<?php echo $post->ID?>&language=<?php echo $lang?>&_nL_nonce=<?php echo wp_create_nonce(__FILE__)?>">Create a new translated <?php echo $post->post_type?></a></p>
-			<?php
-		}
-	}else{
-		// Otherwise, provide a list of posts in the default language to select as the original version
-		$lang = nL_get_post_lang($post->ID);
-		$lang_posts = get_posts(array(
+		// Get a list of available posts in the selected language
+		$lang_posts = new WP_Query(array(
 			'post_type' => $post->post_type,
-			'post_status' => 'publish',
 			'posts_per_page' => -1,
-			'language' => nL_default_lang(),
+			'language' => $lang,
 			'orderby' => 'post_title',
 			'order' => 'ASC',
 		));
 		?>
 		<p>
-			<?php if($op = nL_get_original_post($post->ID, false)):?>
-			<a href="<?php echo admin_url('/post.php')?>?post_type=<?php echo get_post_type($op)?>&post=<?php echo $op?>&action=edit" class="button" target="_blank">Edit "<?php echo $wpdb->get_var("SELECT post_title FROM $wpdb->posts WHERE ID = $op")?>"</a>
-			Or...
-			<?php endif;?>
-		</p>
-		<p><label>
-			Make this a translation of:
-			<select name="translation_of">
-				<option value="-1">Select <?php echo nL_get_lang('name')?> <?php echo $post->post_type?></option>
-			<?php foreach($lang_posts as $lang_post):?>
-				<option value="<?php echo $lang_post->ID?>" <?php if(nL_get_translated_post($lang_post->ID, $lang) == $post->ID) echo 'selected'?>><?php echo $lang_post->post_title?></option>
+			<strong><?php echo $data['name']?>:</strong>
+			<select name="translations[<?php echo $lang?>]">
+				<option value="-1">None</option>
+			<?php foreach($lang_posts->posts as $lang_post):?>
+				<option value="<?php echo $lang_post->ID?>" <?php if($lang_post->ID == $translation) echo 'selected'?>><?php echo $lang_post->post_title?></option>
 			<?php endforeach;?>
 			</select>
-		</label></p>
+			or <a href="<?php echo admin_url()?>?nL_new_translation=<?php echo $post->ID?>&language=<?php echo $lang?>&_nL_nonce=<?php echo wp_create_nonce(__FILE__)?>" class="button-secondary">Create a new <?php echo strtolower($data['name'])?> <?php echo str_replace('_', '', $post->post_type)?></a>
+		</p>
 		<?php
 	}
 }
@@ -118,28 +79,12 @@ function nLingual_save_post($post_id, $post){
 
 	// Set the language
 	if($_POST['language']){
-		wp_set_object_terms($post_id, intval($_POST['language']), 'language');
+		wp_set_object_terms($post_id, $_POST['language'], 'language');
+		nL_set_post_lang($post_id, $_POST['language']);
 	}
 
 	if(isset($_POST['translations'])){
-		// Update the translation status of each post selected
-		foreach($_POST['translations'] as $lang => $id){
-			update_post_meta($post_id, "_translated_$lang", $id);
-		}
-	}
-
-	if(isset($_POST['translation_of'])){
-		// Update the translation status of this post
-		$target = $_POST['translation_of'];
-
-		$lang = nL_get_post_lang($post_id);
-		if($lang == nL_default_lang()) return;
-
-		if($target == -1){
-			$wpdb->query($wpdb->prepare("DELETE FROM $wpdb->postmeta WHERE meta_key = %s AND meta_value = %d", "_translated_$lang", $post_id));
-		}else{
-			update_post_meta($target, "_translated_$lang", $post_id);
-		}
+		nL_associate_posts($post_id, $_POST['translations']);
 	}
 
 	// Loop through the sync options, and syncronize the fields with it's associated posts
@@ -169,11 +114,10 @@ function nLingual_new_translation(){
 		if(!isset($_GET['_nL_nonce']) || !wp_verify_nonce($_GET['_nL_nonce'], __FILE__))
 			wp_die('You do not have permission to do that.');
 
-		if(!nL_in_default_lang($post_id))
-			wp_die('This object is not in the default language.');
-
-		if(!isset($_GET['language']) || !($lang = get_term_by('slug', $_GET['language'], 'language')))
+		if(!isset($_GET['language']) || !nL_lang_exists($_GET['language']))
 			wp_die('Invalid language.');
+
+		$lang = $_GET['language'];
 
 		// Load the original posts post/meta/tax data
 		$orig = $wpdb->get_row($wpdb->prepare("SELECT post_title, post_type, post_content, post_excerpt, post_parent, menu_order FROM $wpdb->posts WHERE ID = %d", $post_id), ARRAY_A);
@@ -189,7 +133,7 @@ function nLingual_new_translation(){
 		}
 
 		// Set the language term to the requested language
-		$tax_query['language'] = $lang->term_id;
+		$tax_query['language'] = $lang;
 
 		// Build the arguments for wp_insert_args
 		$args = $orig;
@@ -197,25 +141,23 @@ function nLingual_new_translation(){
 
 		// Set the status to draft and update the title to flag it as needing translation
 		$args['post_status'] = 'draft';
-		$args['post_title'] = "Translate to $lang->name: {$args['post_title']}";
+		$args['post_title'] = sprintf('Translate to %s: %s', nL_get_lang('name', $lang), $args['post_title']);
 
 		// Set the post parent to be the translated parent if available
-		$args['post_parent'] = nL_get_translated_post($orig['post_parent'], $lang->slug);
+		$args['post_parent'] = nL_get_translation($orig['post_parent'], $lang->slug);
 
 		// Inser the new post
 		$new = wp_insert_post($args);
 
 		// Loop through the metadata and apply it to the new post (except the _translated_[lang] field, not that that should exist anyway)
 		foreach($orig_meta as $key => $value){
-			if(strpos($key, '_translated_') === false){
-				foreach($value as $val){
-					add_post_meta($new, $key, maybe_unserialize($val));
-				}
+			foreach($value as $val){
+				add_post_meta($new, $key, maybe_unserialize($val));
 			}
 		}
 
 		// Set the translation status
-		update_post_meta($post_id, "_translated_$lang->slug", $new);
+		nL_associate_posts($post_id, array($lang => $new));
 
 		// Redirect them to the edit screen for the new post
 		header('Location: '.admin_url("/post.php?post=$new&action=edit"));
