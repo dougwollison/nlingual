4a643cf4bc79f6f2844500590d02233f6d42bd04
Doug Wollison <doug@ninesides.com>
2013-09-18 11:50:30 -0400

    Localization setup

--- php/admin.settings.php
+++ php/admin.settings.php
@@ -5,7 +5,13 @@
 
 add_action('admin_menu', 'nLingual_options_menu');
 function nLingual_options_menu(){
-	add_menu_page('nLingual Settings', 'nLingual', 'manage_options', 'nLingual', 'nLingual_settings_page');
+	add_menu_page(
+		__('nLingual Settings', NL_TXTDMN),
+		'nLingual',
+		'manage_options',
+		'nLingual',
+		'nLingual_settings_page'
+	);
 }
 
 function nLingual_settings_page(){
@@ -30,8 +36,8 @@ function nLingual_settings_page(){
  */
 add_action('admin_init', 'nLingual_register_settings');
 function nLingual_register_settings(){
-	add_settings_section('nLingual-options', 'Options', 'nLingual_manage_options', 'nLingual');
-	add_settings_section('nLingual-languages', 'Languages', 'nLingual_manage_languages', 'nLingual');
+	add_settings_section('nLingual-options', __('Options', NL_TXTDMN), 'nLingual_manage_options', 'nLingual');
+	add_settings_section('nLingual-languages', __('Languages', NL_TXTDMN), 'nLingual_manage_languages', 'nLingual');
 
 	register_setting('nLingual', 'nLingual-options');
 	register_setting('nLingual', 'nLingual-languages', function($data){
@@ -47,7 +53,7 @@ function nLingual_register_settings(){
 		return $languages;
 	});
 
-	add_settings_field('redirection-method', 'Language redirection method', function(){
+	add_settings_field('redirection-method', __('Language redirection method', NL_TXTDMN), function(){
 		$compare = nL_get_option('method');
 		$options = array(
 			'Subdomain (e.g. <code>%1$s.%2$s</code>)' => NL_REDIRECT_USING_DOMAIN,
@@ -57,21 +63,21 @@ function nLingual_register_settings(){
 
 		foreach($options as $label => $value){
 			$label = sprintf($label, nL_default_lang(), parse_url(get_bloginfo('home'), PHP_URL_HOST));
-			printf('<label><input type="radio" name="nLingual-options[method]" value="%s" %s> %s</label><br/>', $value, $value == $compare ? 'checked' : '', $label);
+			printf('<label><input type="radio" name="nLingual-options[method]" value="%s" %s> %s</label><br/>', $value, $value == $compare ? 'checked' : '', __($label, 'NL_TXTDMN'));
 		}
 	}, 'nLingual', 'nLingual-options');
 
-	add_settings_field('request-vars', 'POST & GET variable names', function(){
+	add_settings_field('request-vars', __('POST & GET variable names', NL_TXTDMN), function(){
 		$get = nL_get_option('get_var');
 		$post = nL_get_option('post_var');
 
-		echo '<label>If these parameters are passed, they will override the language set via the redirection method.</label><br>';
+		printf('<label>%s</label><br>', __('If these parameters are passed, they will override the language set via the redirection method.', NL_TXTDMN));
 
 		printf('<label><code>GET&nbsp;</code> <input type="text" name="nLingual-options[get_var]" value="%s"></label><br>', $get);
 		printf('<label><code>POST</code> <input type="text" name="nLingual-options[post_var]" value="%s"></label><br>', $post);
 	}, 'nLingual', 'nLingual-options');
 
-	add_settings_field('post_types', 'Supported post types', function(){
+	add_settings_field('post_types', __('Supported post types', NL_TXTDMN), function(){
 		$post_types = nL_post_types();
 
 		$available = get_post_types(array('show_ui' => true, 'show_in_nav_menus' => true), 'objects');
@@ -81,19 +87,19 @@ function nLingual_register_settings(){
 		}
 	}, 'nLingual', 'nLingual-options');
 
-	add_settings_field('split_separator', 'Split language separator', function(){
+	add_settings_field('split_separator', __('Split language separator', NL_TXTDMN), function(){
 		$separator = nL_get_option('separator');
 
 		printf('<input id="split_separator" type="text" class="small-text" name="nLingual-options[separator]" value="%s">', $separator);
-		echo '<p class="description">Used for splitting things like the blog title and extracting the title for the appropriate language.</p>';
-		printf('<p class="description">Example: <strong>English Title %1$s French Title %1$s Spanish Title</strong> (if the language order is English, French, Spanish)</p>', $separator);
+		printf('<p>%s</p>', __('Used for splitting things like the blog title and extracting the title for the appropriate language.', NL_TXTDMN));
+		printf('<p class="description">'.__('Example: <strong>English Title %1$s French Title %1$s Spanish Title</strong> (if the language order is English, French, Spanish)', NL_TXTDMN).'</p>', $separator);
 	}, 'nLingual', 'nLingual-options');
 
-	add_settings_field('l10n_dateformat', 'Localize date format?', function(){
+	add_settings_field('l10n_dateformat', __('Localize date format?', NL_TXTDMN), function(){
 		$bool = get_option('l10n_dateformat');
 
-		printf('<label><input id="l10n_dateformat" type="checkbox" name="nLingual-options[l10n_dateformat]" value="1" %s> Run localization on the date format string</label>', $bool ? 'checked' : 'checked');
-		echo '<p class="description">Use if any of your languages use custom date formats.</p>';
+		printf('<label><input id="l10n_dateformat" type="checkbox" name="nLingual-options[l10n_dateformat]" value="1" %s> %s</label>', $bool ? 'checked' : 'checked', __('Run localization on the date format string', NL_TXTDMN));
+		printf('<p class="description">%s</p>', __('Use if any of your languages use custom date formats.', NL_TXTDMN));
 	}, 'nLingual', 'nLingual-options');
 }
 
@@ -104,7 +110,7 @@ function nLingual_manage_options(){
 function nLingual_manage_languages(){
 	$languages = nL_languages();
 	?>
-	<button id="nLingual_add_language" type="button" class="button-secondary">Add Language</button>
+	<button id="nLingual_add_language" type="button" class="button-secondary"><?php _e('Add Language', NL_TXTDMN)?></button>
 
 	<div id="nLingual_languages">
 	<?php foreach($languages as $language) _nLingual_language_editor($language)?>
@@ -123,15 +129,30 @@ function _nLingual_language_editor($language = array()){
 	<div class="language">
 		<div class="controls">
 			<label class="default"><input type="radio" name="nLingual-options[default_lang]" value="<?php echo $iso?>" <?php if($default == $iso) echo 'checked'?>> Default</label>
-			<button type="button" class="delete button-secondary">Delete</button>
+			<button type="button" class="delete button-secondary"><?php _e('Delete', NL_TXTDMN)?></button>
 			<br class="clearfix">
 		</div>
 		<div class="info">
-			<label class="name">System Name <input type="text" name="nLingual-languages[name][]" value="<?php echo $name?>"></label>
-			<label class="native">Native Name <input type="text" name="nLingual-languages[native][]" value="<?php echo $native?>"></label>
-			<label class="iso" title="The 2 letter code to use for identifying the langauge.">ISO <input type="text" name="nLingual-languages[iso][]" value="<?php echo $iso?>" maxlength="2"></label>
-			<label class="mo" title="The name (minus extension) of the .MO file use for localization.">.MO file <input type="text" name="nLingual-languages[mo][]" value="<?php echo $mo?>"></label>
-			<label class="tag" title="A shorthand name for the language.">Short name <input type="text" name="nLingual-languages[tag][]" value="<?php echo $tag?>"></label>
+			<label class="name">
+				<?php _e('System Name', NL_TXTDMN)?>
+				<input type="text" name="nLingual-languages[name][]" value="<?php echo $name?>">
+			</label>
+			<label class="native">
+				<?php _e('Native Name', NL_TXTDMN)?>
+				<input type="text" name="nLingual-languages[native][]" value="<?php echo $native?>">
+			</label>
+			<label class="iso" title="<?php _e('The 2 letter code to use for identifying the langauge.', NL_TXTDMN)?>">
+				<?php _e('ISO', NL_TXTDMN)?>
+				<input type="text" name="nLingual-languages[iso][]" value="<?php echo $iso?>" maxlength="2">
+			</label>
+			<label class="mo" title="<?php _e('The name (minus extension) of the .MO file use for localization.', NL_TXTDMN)?>">
+				<?php _e('.MO file', NL_TXTDMN)?>
+				<input type="text" name="nLingual-languages[mo][]" value="<?php echo $mo?>">
+			</label>
+			<label class="tag" title="<?php _e('A shorthand name for the language.', NL_TXTDMN)?>">
+				<?php _e('Short name', NL_TXTDMN)?>
+				<input type="text" name="nLingual-languages[tag][]" value="<?php echo $tag?>">
+			</label>
 			<br class="clearfix">
 		</div>
 	</div>
