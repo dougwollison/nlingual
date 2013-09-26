a1ac64c6ca67675503d854ad0433cf3f6489393c
Doug Wollison <doug@ninesides.com>
2013-09-26 11:22:33 -0400

    Added language short codes

---
+++ php/public/shortcodes.php
@@ -0,0 +1,32 @@
+<?php
+// ===================== //
+//	Language Shortcodes  //
+// ===================== //
+
+/*
+ * Add filter for unwrapping language shortcodes
+ */
+add_filter('the_content', 'nLingual_shortcodes');
+function nLingual_shortcodes($content){
+	global $languages;
+	//Strip closing p tags and opening p tags from beginning/end of string
+	$content = preg_replace('#^\s*(?:</p>)\s*([\s\S]+)\s*(?:<p.*?>)\s*$#', '$1', $content);
+	//Unwrap tags
+	$content = preg_replace('#(?:<p.*?>)?(\[/?(?:'.implode('|', array_keys(nL_languages())).').*\])(?:</p>)?#', '$1', $content);
+
+	return trim($content);
+}
+
+/*
+ * Add a shortcode for each language slug
+ */
+foreach(nL_languages() as $lang){
+	add_shortcode($lang['slug'], 'nLingual_show_language');
+}
+/*
+ * Language shortcode; return the containing text ONLY if it's for the current language
+ */
+function nLingual_show_language($atts, $content, $lang){
+	if($lang == nL_current_lang()) return trim(do_shortcode($content));
+	return null;
+}
