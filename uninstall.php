9264aeb481e167386e447b4057a18f60e63f5b13
Doug Wollison <doug@ninesides.com>
2013-09-26 14:31:19 -0400

    Added uninstall script.

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+// Abort if WordPress isn't actually uninstalling anything
+if(!defined('WP_UNINSTALL_PLUGIN')) exit;
+
+// Or if the user can't activate plugins
+if(!current_user_can('activate_plugins'))
+	return;
+
+// Or if this isn't the plugin we're supposed to uninstall
+if(WP_UNINSTALL_PLUGIN != 'nLingual/nLingual.php')
+	return;
+
+global $wpdb, $table_prefix;
+
+// Now, delete the translations table if it exists
+$wpdb->query("DROP TABLE `{$table_prefix}nL_translations`");
+
+// Oh, and delete the options
+delet_option('nLingual-options');
+delet_option('nLingual-sync_rules');
+delet_option('nLingual-languages');
