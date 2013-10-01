b8ef25eb5e235bc9af28c4af7819ebd3b80be246
Doug Wollison <doug@wollison.net>
2013-09-30 21:39:13 -0400

    Updated detection hook to use modified process_url

--- php/public/detection.php
+++ php/public/detection.php
@@ -19,11 +19,11 @@ function nLingual_detect_requested_language(){
 	}
 
 	// Process the $here url (within nLingual) and get the language
-	if($result = nL_process_url()){
-		$lang = $result['lang'];
+	if($processed = nL_process_url()){
+		$lang = $processed['lang'];
 		// Update host & uri with processed versions
-		$_SERVER['HTTP_HOST'] = $result['host'];
-		$_SERVER['REQUEST_URI'] = $result['path'].($result['query'] ? '?'.$result['query'] : '');
+		$_SERVER['HTTP_HOST'] = $processed['host'];
+		$_SERVER['REQUEST_URI'] = $processed['path'].($processed['query'] ? '?'.$processed['query'] : '');
 	}
 
 	// Override with get_var method if present and valid
