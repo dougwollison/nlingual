fd920af0610d734d21a2cc9d910345478741c0cc
Doug Wollison <doug@wollison.net>
2015-04-04 16:37:30 -0400

    Added hook for qs-context queries, cleaned up code and commenting.

--- php/global/quickstart.php
+++ php/global/quickstart.php
@@ -6,8 +6,10 @@
 if ( class_exists( 'QuickStart' ) ) {
 	// Add hooks to support the index page feature
 	if ( current_theme_supports( 'quickstart-index_page' ) ) {
+		// Setup translation of the get_index helper function
 		add_filter('qs_helper_get_index', 'nL_get_translation');
 
+		// Add handling of custom index pages for URL localizing
 		function nL_qs_index_localize( $link, $lang ) {
 			if ( ! is_post_type_archive() ) return $link;
 
@@ -21,7 +23,22 @@ if ( class_exists( 'QuickStart' ) ) {
 			}
 			return $link;
 		}
-
 		add_filter( 'nLingual_localize_here', 'nL_qs_index_localize', 10, 2 );
 	}
+	
+	// Add language filtering to queries made by QuickStart (i.e. order manager and parent filtering)
+	function nL_qs_admin_parse_query( $query ) {
+		if ( ! is_admin() || ! isset( $query->query_vars['qs-context'] ) ) {
+			return;
+		}
+	
+		$qvar = nL_query_var();
+		$pt = isset( $query->query_vars['post_type'] ) ? $query->query_vars['post_type'] : null;
+	
+		if ( nL_post_type_supported( $pt )
+		&& ! isset( $query->query_vars[ $qvar ] ) ) {
+			$query->query_vars[ $qvar ] = nL_current_lang();
+		}
+	}
+	add_filter( 'parse_query', 'nL_qs_admin_parse_query' );
 }
