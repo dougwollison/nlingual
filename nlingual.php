ae17b7cf4a40d52d9fd48439636f404d76e1220d
Doug Wollison <Doug@Dougs-iMac.local>
2015-09-16 20:12:23 -0400

    Starting over.

--- nlingual.php
+++ nlingual.php
@@ -3,15 +3,15 @@
 Plugin Name: nLingual
 Plugin URI: https://github.com/dougwollison/nLingual
 Description: Easy to manage Multilingual system, with theme development utilities and post data synchronization.
-Version: 1.3.1
+Version: 2.0.0
 Author: Doug Wollison
 Author URI: http://dougw.me
 Tags: multilingual, multi, language, admin, bilingual, switcher, translation, nlingual
 License: GPL2
 */
 
+// Constants
 define( 'NL_SELF', __FILE__ );
+define( 'NL_DIR', __DIR__ );
 define( 'NL_TXTDMN', 'nLingual' );
-define( 'NL_DB_VERSION', '1.2.3' );
-
-require( __DIR__ . '/inc/load.php' );
+define( 'NL_DB_VERSION', '2.0.0' );
