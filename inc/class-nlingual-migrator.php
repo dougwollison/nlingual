322c27778c6134d903be9cfbdc4842e1efbd5cee
Doug Wollison <doug@wollison.net>
2015-09-18 10:48:55 -0400

    Renamed Migration to Migrator. Because.

--- inc/class-nlingual-migrator.php
+++ inc/class-nlingual-migrator.php
@@ -22,25 +22,28 @@ class Migration {
 
 		static::export_languages();
 
-		// Delete the table
-		$wpdb->query( "DROP TABLE {$wpdb->prefix}nL_languages" );
+		// Get the collation if applicable
+		$collate = '';
+		if ( ! empty( $wpdb->collate ) ) {
+			$collate = "COLLATE $wpdb->collate";
+		}
 
 		// We need to alter the translations table to the new format
-		$wpdb->query(
-			// Rename to new lowercase naming scheme
-			"ALTER TABLE {$wpdb->prefix}nL_translations RENAME TO $wpdb->nl_translations;".
-			// Start by removing the old unique key
-			"ALTER TABLE $wpdb->nl_translations DROP KEY `post_id`;".
-			// Now add the new object_type column
-			"ALTER TABLE $wpdb->nl_translations ADD `object_type` varchar(20) COLLATE $charset_collate NOT NULL DEFAULT 'post';".
-			// Rename post_id to object_id, placing it after object_type
-			"ALTER TABLE $wpdb->nl_translations CHANGE `post_id` `object_id` bigint(20) unsigned NOT NULL AFTER `object_type`;".
-			// Add the new unique key for object type/id pairs
-			"ALTER TABLE $wpdb->nl_translations ADD UNIQUE KEY object_type_id (object_id, object_type);"
-		);
+
+		// Rename to lowercase
+		$wpdb->query("ALTER TABLE {$wpdb->prefix}nL_translations RENAME TO {$wpdb->prefix}nl_translations2");
+		$wpdb->query("ALTER TABLE {$wpdb->prefix}nl_translations2 RENAME TO $wpdb->nl_translations");
+		// Start by removing the old unique key
+		$wpdb->query("ALTER TABLE $wpdb->nl_translations DROP KEY `post_id`");
+		// Now add the new object_type column
+		$wpdb->query("ALTER TABLE $wpdb->nl_translations ADD `object_type` varchar(20) $collate NOT NULL DEFAULT 'post'");
+		// Rename post_id to object_id, placing it after object_type
+		$wpdb->query("ALTER TABLE $wpdb->nl_translations CHANGE `post_id` `object_id` bigint(20) unsigned NOT NULL AFTER `object_type`");
+		// Add the new unique key for object type/id pairs
+		$wpdb->query("ALTER TABLE $wpdb->nl_translations ADD UNIQUE KEY object_type_id (object_id, object_type)");
 
 		// Just in case, mark them down as being at least up to 2.0.0
-		update_option( 'nlingual_database_version', '2.0.0' );
+		//update_option( 'nlingual_database_version', '2.0.0' );
 	}
 
 	/**
@@ -53,6 +56,13 @@ class Migration {
 	public static function export_languages() {
 		global $wpdb;
 
+		$table = $wpdb->prefix.'nL_languages';
+
+		// Check if old languages table exists
+		if ( ! $wpdb->get_var("SHOW TABLES LIKE '$table'") ) {
+			return;
+		}
+
 		// Export the contents of the old languages table
 		$data = $wpdb->get_results("
 			SELECT
@@ -64,9 +74,12 @@ class Migration {
 				slug,
 				iso,
 				mo
-			FROM {$wpdb->prefix}nL_languages
+			FROM $table
 			ORDER BY list_order
 		");
+
+		// Delete the table
+		$wpdb->query("DROP TABLE $table");
 
 		// Convert to new structure
 		$languages = new Languages;
