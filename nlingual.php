fd8b144fa5268c10a160eb2cf6778deca74ce32b
Doug Wollison <Doug@Dougs-iMac.local>
2015-09-22 22:19:54 -0400

    More constants/flags (now all located here)

--- nlingual.php
+++ nlingual.php
@@ -15,6 +15,13 @@ define( 'NL_SELF', __FILE__ );
 define( 'NL_DIR', __DIR__ );
 define( 'NL_TXTDMN', 'nLingual' );
 define( 'NL_DB_VERSION', '2.0.0' );
+define( 'NL_ORIGINAL_URL', ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
+
+// Flags
+define( 'NL_UNLOCALIZED', 'NL_UNLOCALIZED' );
+define( 'NL_REDIRECT_USING_GET', 'NL_REDIRECT_USING_GET' );
+define( 'NL_REDIRECT_USING_PATH', 'NL_REDIRECT_USING_PATH' );
+define( 'NL_REDIRECT_USING_DOMAIN', 'NL_REDIRECT_USING_DOMAIN' );
 
 // Load includes
 require( NL_DIR . '/inc/utilities.php' );
