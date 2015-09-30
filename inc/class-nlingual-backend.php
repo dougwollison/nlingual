b1ff2ac01aaa927b897c7f32a52ec5fc3fc210e9
Doug Wollison <Doug@Dougs-iMac.local>
2015-09-30 00:18:59 -0400

    Started adding admin pages and settings/fields.

--- inc/class-nlingual-backend.php
+++ inc/class-nlingual-backend.php
@@ -73,6 +73,10 @@ class Backend extends Functional {
 
 		// JavaScript Variables
 		static::add_action( 'admin_footer', 'print_javascript_vars' );
+
+		// Settings Pages
+		static::add_action( 'admin_init', 'register_settings' );
+		static::add_action( 'admin_menu', 'add_menu_pages' );
 	}
 
 	// =========================
@@ -778,6 +782,206 @@ class Backend extends Functional {
 			}
 			var NL_LANGUAGES = <?php echo json_encode( Registry::languages()->export() ); ?>
 		</script>
+		<?php
+	}
+
+	// =========================
+	// ! Settings Reg/Output
+	// =========================
+
+	/**
+	 * Register the settings/fields for the admin pages.
+	 *
+	 * @since 2.0.0
+	 */
+	public static function register_settings() {
+		/* === Translation Options === */
+
+		Settings::register( array(
+			'default_language'   => 'intval',
+			'skip_default_l10n'  => 'intval',
+			'query_var'          => null,
+			'redirection_method' => null,
+			'postlang_override'  => null,
+		), 'options' );
+
+		add_settings_section( 'default', null, null, 'nlingual-options' );
+
+		Settings::add_fields( array(
+			'default_language' => array(
+				'title' => __( 'Default Language', NLTXTDMN ),
+				'help'  => null,
+				'type'  => 'select',
+				'data'  => Registry::languages()->export( 'system_name' ),
+			),
+			'skip_default_l10n' => array(
+				'title' => __( 'Skip Localization for Default Language?', NLTXTDMN ),
+				'help'  => __( 'URLs for the default language will be unmodified (e.g. /english-page/ vs /en/english-page/).', NLTXTDMN ),
+				'type'  => 'checkbox',
+			),
+		), 'options' );
+
+		add_settings_section( 'redirection', __( 'Request Handling', NLTXTDMN ), null, 'nlingual-options' );
+
+		Settings::add_fields( array(
+			'query_var' => array(
+				'title' => __( 'Query Variable', NLTXTDMN ),
+				'help'  => __( 'The variable name to check for when handling language requests (recommended: "language")', NLTXTDMN ),
+				'type'  => 'input',
+			),
+			'redirection_method' => array(
+				'title' => __( 'Redirection Method', NLTXTDMN ),
+				'help'  => __( 'What style should be used for the translated URLs?', NLTXTDMN ),
+				'type'  => 'radiolist',
+				'data'  => array(
+					NL_REDIRECT_USING_GET    => __( 'HTTP query (e.g. <code>%s/?%s=%s</code>)', NLTXTDMN ),
+					NL_REDIRECT_USING_PATH   => __( 'Path prefix (e.g. <code>%s/%s/</code>)', NLTXTDMN ),
+					NL_REDIRECT_USING_DOMAIN => __( 'Subdomain (e.g. <code>%s.%s</code>)', NLTXTDMN ),
+				),
+			),
+			'postlang_override' => array(
+				'title' => __( 'Post Language Override', NLTXTDMN ),
+				'help'  => __( 'Should the requested post/page/object’s language override the one requested?', NLTXTDMN ),
+				'type'  => 'checkbox',
+			),
+		), 'options', 'redirection' );
+
+		/* === Localizables Options === */
+
+		Settings::register( array(
+			'post_types'   => null,
+			'localizables' => null,
+		), 'l10s' );
+
+		add_settings_section( 'default', null, null, 'nlingual-l10s' );
+
+		// Build the post types list
+		$post_types = array();
+		foreach ( get_post_types( array(
+			'show_ui' => true,
+		), 'objects' ) as $post_type ) {
+			// Automatically skip attachments
+			if ( $post_type->name == 'attachment' ) {
+				continue;
+			}
+			$post_types[ $post_type->name ] = $post_type->labels->name;
+		}
+
+		// Get the nav locations list
+		$nav_locations = get_registered_nav_menus();
+
+		// Build the sidebar locations list
+		$sidebars = array();
+
+		Settings::add_fields( array(
+			'post_types' => array(
+				'title' => __( 'Post Types', NLTXTDMN ),
+				'help'  => __( 'What post types should support language and translations?', NLTXTDMN ),
+				'type'  => 'checklist',
+				'data'  => $post_types,
+			),
+			'localizables[nav_menu_locations]' => array(
+				'title' => __( 'Menu Locations', NLTXTDMN ),
+				'help'  => __( 'Should any/all navigation menu locations have versions for each language?', NLTXTDMN ),
+				'type'  => 'checklist',
+				'data'  => $nav_locations,
+			),
+			'localizables[sidebar_locations]' => array(
+				'title' => __( 'Sidebar Locations', NLTXTDMN ),
+				'help'  => __( 'Should any/all widget sidebar locations have versions for each language?', NLTXTDMN ),
+				'type'  => 'checklist',
+				'data'  => $sidebars,
+			),
+		), 'l10s' );
+	}
+
+	// =========================
+	// ! Settings Pages
+	// =========================
+
+	/**
+	 * Register admin pages.
+	 *
+	 * @since 2.0.0
+	 */
+	public static function add_menu_pages() {
+		add_utility_page(
+			__( 'Translation Options', NLTXTDMN ), // page title
+			_x( 'Translation', 'menu title', NLTXTDMN ), // menu title
+			'manage_options', // capability
+			'nlingual-options', // slug
+			array( static::$name, 'settings_page' ), // callback
+			'dashicons-translation' // icon
+		);
+
+		add_submenu_page(
+			'nlingual-options', // parent
+			__( 'Manage Languages', NLTXTDMN ), // page title
+			_x( 'Languages', 'menu title', NLTXTDMN ), // menu title
+			'manage_options', // capability
+			'nlingual-languages', // slug
+			array( static::$name, 'settings_page_languages' ) // callback
+		);
+
+		add_submenu_page(
+			'nlingual-options', // parent
+			__( 'Manage Localizable Objects', NLTXTDMN ), // page title
+			__( 'Localizables',  NLTXTDMN ), // menu title
+			'manage_options', // capability
+			'nlingual-l10s', // slug
+			array( static::$name, 'settings_page' ) // callback
+		);
+
+		add_submenu_page(
+			'nlingual-options', // parent
+			__( 'Post Synchronization', NLTXTDMN ), // page title
+			__( 'Sync Options', NLTXTDMN ), // menu title
+			'manage_options', // capability
+			'nlingual-sync', // slug
+			array( static::$name, 'settings_page' ) // callback
+		);
+	}
+
+	/**
+	 * Output for generic settings page.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @global $plugin_page The slug of the current admin page.
+	 */
+	public static function settings_page() {
+		global $plugin_page;
+		?>
+		<div class="wrap">
+			<h1><?php echo get_admin_page_title(); ?></h1>
+
+			<form method="post" action="options.php">
+				<?php settings_fields( $plugin_page ); ?>
+				<?php do_settings_sections( $plugin_page ); ?>
+				<?php submit_button(); ?>
+			</form>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Output for the language management page.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @global $plugin_page The slug of the current admin page.
+	 */
+	public static function settings_page_languages() {
+		global $plugin_page;
+		?>
+		<div class="wrap">
+			<h1><?php echo get_admin_page_title(); ?></h1>
+
+			<form method="post" action="options.php">
+				<?php settings_fields( $plugin_page ); ?>
+				<?php submit_button(); ?>
+			</form>
+		</div>
 		<?php
 	}
 }
---
+++ inc/class-nlingual-settings.php
@@ -0,0 +1,267 @@
+<?php
+namespace nLingual;
+
+/**
+ * nLingual Settings Handler
+ *
+ * @package nLingual
+ *
+ * @since 2.0.0
+ */
+
+class Settings {
+	/**
+	 * Register the desired settings.
+	 *
+	 * Prefixes all option names with "nlingual_"
+	 * and the group name with "nlingual-"
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param array  $settings The settings to register, in name => sanitize_callback format.
+	 * @param string $group    The name of the group to register them for.
+	 */
+	public static function register( array $settings, $group ) {
+		foreach ( $settings as $setting => $callback ) {
+			register_setting( "nlingual-{$group}", "nlingual_{$setting}", $callback );
+		}
+	}
+
+	/**
+	 * Add the desired settings field.
+	 *
+	 * Prefixes option name with "nlingual_"
+	 * and the page name with "nlingual-".
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string $field   The name of the field.
+	 * @param array  $options The options for the field.
+	 * 		@option string "title" The title of the field.
+	 * 		@option string "label" Additional label for the input.
+	 *		@option string "help"  The help text for the field.
+	 *		@option string "type"  The type of field to print out.
+	 *		@option string "args"  Special arguments for the field callback.
+	 * @param string $page    The name of the page to display on.
+	 * @param string $section Optional The name of the section to display in.
+	 */
+	public static function add_field( $field, $options, $page, $section = 'default' ) {
+		// Parse the options
+		wp_parse_args( $options, array(
+			'title' => '',
+			'label' => '',
+			'help'  => '',
+			'type'  => '',
+			'data'  => array()
+		) );
+
+		// Build the callback arguments
+		$args = array(
+			'class'     => "nlingual_{$field}-field",
+			'name'      => "nlingual_{$field}",
+			'label'     => $options['label'],
+			'help'      => $options['help'],
+			'type'      => $options['type'],
+			'data'      => $options['data'],
+		);
+
+		// Add label_for arg if appropriate
+		if ( ! in_array( $options['type'], array( 'radiolist', 'checklist', 'checkbox' ) ) ) {
+			$args['label_for'] = "nlingual_{$field}";
+		}
+
+		// Add the settings field
+		add_settings_field(
+			"nlingual_{$field}", // id
+			$options['title'], // title
+			array( get_called_class(), 'build_field' ), // callback
+			"nlingual-{$page}", // page
+			$section, // section
+			$args // arguments
+		);
+	}
+
+	/**
+	 * Add multiple settings fields.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @see Settings::build_field() for how the fields are built.
+	 *
+	 * @param array  $fields  The fields to add.
+	 * @param string $page    The name of the page to display on.
+	 * @param string $section Optional The name of the section to display in.
+	 */
+	public static function add_fields( $fields, $page, $section = 'default' ) {
+		foreach ( $fields as $field => $options ) {
+			static::add_field( $field, $options, $page, $section );
+		}
+	}
+
+	/**
+	 * Build a field.
+	 *
+	 * Calls appropriate build_%type%_field method,
+	 * along with printing out help text.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param array $args The arguments for the field.
+	 *		@option string "name" The field name/ID.
+	 *		@option string "type" The field type.
+	 *		@option mixed  "data" Optional data for the field.
+	 * 		@option string "help" Optional Help text.
+	 */
+	public static function build_field( $args ) {
+		switch ( $args['type'] ) {
+			case 'select':
+			case 'radiolist':
+			case 'checklist':
+				$method = "build_{$args['type']}_field";
+				$cb_args = array( $args['name'], $args['data'] );
+				break;
+
+			default:
+				$method = "build_input_field";
+				$cb_args = array( $args['name'], $args['type'], $args['label'], $args['data'] );
+		}
+
+		$html = call_user_func_array( array( get_called_class(), $method ), $cb_args );
+
+		if ( $args['help'] ) {
+			// Wrap $html in lable with help text if checkbox or radio
+			if ( $args['type'] == 'checkbox' || $args['type'] == 'radio' ) {
+				$html = sprintf( '<label>%s %s</label>', $html, $args['help'] );
+			} else {
+				// Append as description paragraph otherwise
+				$html .= sprintf( '<p class="description">%s</p>', $args['help'] );
+			}
+		}
+
+		// Print the output
+		echo $html;
+	}
+
+	/**
+	 * Build a basic <input> field.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string $name       The name/ID of the field.
+	 * @param string $label      Optional The label for the input.
+	 * @param array  $attributes Optional Custom attributes for the field.
+	 *
+	 * @return string The HTML of the field.
+	 */
+	protected static function build_input_field( $name, $type, $attributes = array() ) {
+		$html = '';
+
+		// Load the option matching the field name
+		$value = get_option( $name );
+
+		// Create an ID out of the name
+		$id = sanitize_key( $name );
+
+		// If a checkbox, include a default=0 dummy field
+		if ( $type == 'checkbox' ) {
+			$html .= sprintf( '<input type="hidden" name="%s" value="0" />', $name );
+
+			// Also add the checked attribute if $value is true-ish
+			if ( $value ) {
+				$attributes['checked'] = true;
+			}
+
+			// Replace $value with the TRUE (1)
+			$value = 1;
+		}
+
+		// Build the $attributes list if needed
+		$atts = '';
+		foreach ( $attributes as $key => $val ) {
+			if ( is_bool( $val ) ) {
+				$val = $val ? $key : '';
+			}
+
+			$atts .= " {$key}=\"{$val}\"";
+		}
+
+		// Build the input
+		$html .= sprintf( '<input type="%s" name="%s" id="%s" value="%s"%s />', $type, $name, $id, $value, $atts );
+
+		return $html;
+	}
+
+	/**
+	 * Build a <select> field.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string $name    The name/ID of the field.
+	 * @param array  $options The options for the field.
+	 */
+	protected static function build_select_field( $name, $options ) {
+		$html = '';
+
+		// Load the option matching the field name
+		$value = get_option( $name );
+
+		// Create an ID out of the name
+		$id = sanitize_key( $name );
+
+		$html .= sprintf( '<select name="%s" id="%s">', $name, $id );
+		foreach ( $options as $val => $label ) {
+			$selected = $val == $value ? ' selected' : '';
+			$html .= sprintf( '<option value="%s">%s</option>', $val, $label );
+		}
+		$html .= '</select>';
+
+		return $html;
+	}
+
+	/**
+	 * Build a list of inputs.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string $type    The input type.
+	 * @param string $name    The name/ID of the field.
+	 * @param array  $options The options for the field.
+	 */
+	protected static function build_inputlist_field( $type, $name, $options ) {
+		// Load the option matching the field name
+		$value = (array) get_option( $name ); // ensure it's an array
+
+		// Checkbox field support array value
+		if ( $type == 'checkbox' ) {
+			$name .= '[]';
+		}
+
+		$inputs = array();
+		foreach ( $options as $val => $label ) {
+			$checked = in_array( $val, $value ) ? ' checked' : '';
+			$inputs[] = sprintf( '<label><input type="%s" name="%s" value="%s"%s /> %s</label>', $type, $name, $val, $checked, $label );
+		}
+
+		$html = '<fieldset>' . implode( '<br /> ', $inputs ) . '</fieldset>';
+
+		return $html;
+	}
+
+	/**
+	 * Build a list of radio inputs.
+	 *
+	 * @see Settings::build_inputlist_field() for what it all does.
+	 */
+	protected static function build_radiolist_field( $name, $options ) {
+		return static::build_inputlist_field( 'radio', $name, $options );
+	}
+
+	/**
+	 * Build a list of checkbox inputs.
+	 *
+	 * @see Settings::build_input_list() for what it all does.
+	 */
+	protected static function build_checklist_field( $name, $options ) {
+		return static::build_inputlist_field( 'checkbox', $name, $options );
+	}
+}
