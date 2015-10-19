b1d3261420ec52e3f53ecfabc98e2cb54e575b43
Doug Wollison <doug@wollison.net>
2015-10-19 16:46:00 -0400

    Change doc html files to php, added initial post/term editor doc files.

---
+++ doc/term-localization/sidebar.php
@@ -0,0 +1,7 @@
+<strong>Manage Translation System:</strong>
+
+<ul>
+	<li><a href="admin.php?page=nlingual-localizables" target="_blank">Localizeable objects</a></li>
+	<li><a href="admin.php?page=nlingual-languages" target="_blank">Registered languages</a></li>
+	<li><a href="admin.php?page=nlingual-strings" target="_blank">Localized strings</a></li>
+</ul>
