c8d6f53a9a782df793486c2d49f5df61487fb662
Doug Wollison <doug@wollison.net>
2015-10-20 12:10:23 -0400

    Don't want to risk overriding existing sidebar

--- doc/post-translation/sidebar.php
+++
@@ -1,7 +0,0 @@
-<strong>Manage Translation System:</strong>
-
-<ul>
-	<li><a href="admin.php?page=nlingual-localizables" target="_blank">Localizeable objects</a></li>
-	<li><a href="admin.php?page=nlingual-sync" target="_blank">Synchronizing and cloning options</a></li>
-	<li><a href="admin.php?page=nlingual-languages" target="_blank">Registered languages</a></li>
-</ul>
