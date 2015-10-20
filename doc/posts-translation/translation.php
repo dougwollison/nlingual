c3ce4e718bb6093a9f6ee780e0381e22d2c823ff
Doug Wollison <doug@wollison.net>
2015-10-20 12:10:42 -0400

    Documentation for post(s) translation

---
+++ doc/posts-translation/translation.php
@@ -0,0 +1,12 @@
+<?php
+$post_type = get_current_screen()->post_type;
+$singular = strtolower( get_post_type_object( $post_type )->labels->singular_name );
+$plural = strtolower( get_post_type_object( $post_type )->labels->name );
+?>
+On this screen you can view at a glance what language every post is in, as well as what it's translations are. You can also filter the list to show only <?php echo $plural; ?> of a particular language.
+
+The <strong>Quick Edit</strong> option will allow you to quickly change the language or assign translations to a <?php echo $singular; ?>, while Bulk Edit will allow you to change the language of many <?php echo $plural; ?> at once.
+
+If you make changes to a <?php echo $singular; ?> from here, certain fields and settings will be copied to it’s sister translations, synchronizing them. The exact fields/settings that will be synchronized is controlled <a href="<?php echo admin_url('admin.php?page=nlingual-sync'); ?>" target="_blank">here</a>.
+
+<?php require( NL_DIR . '/doc/shared/post-sync-summary.php' ); ?>
---
+++ doc/shared/post-sync-summary.php
@@ -0,0 +1,41 @@
+<?php
+namespace nLingual;
+
+if ( $rules = Registry::get_rules( 'sync', 'post_type', $post_type ) ) :
+	?>
+	<hr />
+	<strong><?php _ef( 'The following details will be synchronized between sister %s', $plural ); ?></strong>
+	<ul>
+		<?php if ( $rules['post_fields'] ) : ?>
+		<li><strong><?php _e( 'Post Data' ); ?></strong> - <?php
+			$post_fields = array();
+			// Get the names of each field
+			foreach ( $rules['post_fields'] as $post_field ) {
+				$post_fields[] = _x( Documenter::$post_field_names[ $post_field ], 'post field' );
+			}
+			echo implode( ', ', $post_fields );
+		?></li>
+		<?php endif; ?>
+
+		<?php if ( $rules['post_terms'] ) : ?>
+		<li><strong><?php _e( 'Taxonomies' ); ?></strong> - <?php
+			$taxonomies = array();
+			// Get the names of each field
+			foreach ( $rules['post_terms'] as $taxonomy ) {
+				$taxonomies[] = get_taxonomy( $taxonomy )->labels->name;
+			}
+			echo implode( ', ', $taxonomies );
+		?></li>
+		<?php endif; ?>
+
+		<?php if ( $rules['post_meta'] ) : ?>
+		<li><strong><?php _e( 'Meta Data' ); ?></strong> - <?php
+			if ( in_array( '*', $rules['post_meta'] ) ) {
+				echo _( 'All custom fields found.' );
+			} else {
+				echo implode( ', ', $rules['post_meta'] );
+			}
+		?></li>
+		<?php endif; ?>
+	</ul>
+<?php endif; ?>
