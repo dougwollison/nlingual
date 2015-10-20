2dab729a7890b4c182aebf1a0caafce5c6051b3e
Doug Wollison <doug@wollison.net>
2015-10-20 12:10:00 -0400

    Updated to use admin_url

--- doc/localizables/sidebar.php
+++ doc/localizables/sidebar.php
@@ -1,6 +1,6 @@
 <strong>Manage Locations:</strong>
 
 <ul>
-	<li><a href="nav-menus.php" target="_blank">Navigation Menus</a></li>
-	<li><a href="widgets.php" target="_blank">Sidebars</a></li>
+	<li><a href="<?php echo admin_url( 'nav-menus.php' ); ?>" target="_blank">Navigation Menus</a></li>
+	<li><a href="<?php echo admin_url( 'widgets.php' ); ?>" target="_blank">Sidebars</a></li>
 </ul>
--- doc/options/sidebar.php
+++ doc/options/sidebar.php
@@ -1,8 +1,8 @@
 <strong>Also manageable:</strong>
 
 <ul>
-	<li><a href="admin.php?page=nlingual-localizables" target="_blank">Localizeable objects</a></li>
-	<li><a href="admin.php?page=nlingual-sync" target="_blank">Synchronizing and cloning options</a></li>
-	<li><a href="admin.php?page=nlingual-languages" target="_blank">Registered languages</a></li>
-	<li><a href="admin.php?page=nlingual-strings" target="_blank">Localized strings</a></li>
+	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-localizables' ); ?>" target="_blank">What should be localizable.</a></li>
+	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-sync' ); ?>" target="_blank">Synchronizing and cloning options.</a></li>
+	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-languages' ); ?>" target="_blank">What languages to use.</a></li>
+	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-strings' ); ?>" target="_blank">Translate strings.</a></li>
 </ul>
