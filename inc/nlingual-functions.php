836dda0ba7fb91bb44ec8cfdc6d1e15a1a5c0e2a
Doug Wollison <doug@wollison.net>
2015-10-22 11:45:00 -0400

    Added is_backend() helper

--- inc/nlingual-functions.php
+++ inc/nlingual-functions.php
@@ -10,6 +10,25 @@
 namespace nLingual;
 
 /**
+ * Check if we're in the backend of the site (excluding frontend AJAX requests)
+ *
+ * @since 2.0.0
+ *
+ * @global string $pagenow The current page slug.
+ */
+function is_backend() {
+	global $pagenow;
+
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		// AJAX request, check if the referrer is from wp-admin
+		return strpos( $_SERVER['HTTP_REFERER'], admin_url() ) === 0;
+	} else {
+		// Check if in the admin or otherwise the login/register page
+		return is_admin() || in_array( $pagenow, array( 'wp-login.php', 'wp-register.php' ) );
+	}
+}
+
+/**
  * Triggers the standard "Cheatin’ uh?" wp_die message.
  *
  * @since 2.0.0
