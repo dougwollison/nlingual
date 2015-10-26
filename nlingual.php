6c4549451806189a1ba1ae60732abf5a76bb6921
Doug Wollison <doug@wollison.net>
2015-10-26 12:16:56 -0400

    Added update notice system.

--- nlingual.php
+++ nlingual.php
@@ -15,3 +15,29 @@ define( 'NL_TXTDMN', 'nLingual' );
 define( 'NL_DB_VERSION', '1.2.3' );
 
 require( __DIR__ . '/inc/load.php' );
+
+// Check for update notices
+if ( is_admin() ) {
+	add_action( 'in_plugin_update_message-' . basename( __DIR__ ) . '/' . basename( __FILE__ ), 'nlingual_update_notice_check' );
+	function nlingual_update_notice_check( $plugin ) {
+		// get the version number
+		$version = $plugin['new_version'];
+
+		// check for a notice on the SVN repo
+		$key = "nlingual_update_notice_$version";
+		$data = get_transient( $key );
+		if ( $data === false ) {
+			// fetch the data and save it
+			$data = file_get_contents( "http://plugins.svn.wordpress.org/nlingual/assets/notice-$version.txt" );
+			if ( ! $data ) {
+				$data = '';
+			}
+			set_transient( $key, $data, YEAR_IN_SECONDS );
+		}
+
+		// if there's a notice, print it out
+		if ( $data ) {
+			echo apply_filters( 'the_content', $data );
+		}
+	}
+}
