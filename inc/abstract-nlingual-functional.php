aebbbeeca8723dd9c02524aa9066dbc2f8435b28
Doug Wollison <doug@wollison.net>
2015-11-04 18:00:35 -0500

    Added maybe_add_filter method (and action alias)

--- inc/abstract-nlingual-functional.php
+++ inc/abstract-nlingual-functional.php
@@ -23,6 +23,8 @@ abstract class Functional {
 	 * Add an internal method to a filter hook.
 	 *
 	 * @since 2.0.0
+	 *
+	 * @see add_filter() for arguement defails.
 	 */
 	final public static function add_filter( $tag, $method, $priority = 10, $accepted_args = 1 ) {
 		add_filter( $tag, array( static::$name, $method ), $priority, $accepted_args );
@@ -41,6 +43,8 @@ abstract class Functional {
 	 * Remove an internal method to a filter hook.
 	 *
 	 * @since 2.0.0
+	 *
+	 * @see remove_filter() for arguement defails.
 	 */
 	final public static function remove_filter( $tag, $method, $priority = 10 ) {
 		remove_filter( $tag, array( static::$name, $method ), $priority );
@@ -53,6 +57,28 @@ abstract class Functional {
 	 */
 	final public static function remove_action() {
 		call_user_func_array( 'self::remove_filter', func_get_args() );
+	}
+
+	/**
+	 * Add an internal method to a filter hook if it hasn't already been.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @see add_filter() for arguement defails.
+	 */
+	final public static function maybe_add_filter( $tag, $method ) {
+		if ( ! has_filter( $tag, array( static::$name, $method ) ) ) {
+			call_user_func_array( 'self::add_filter', func_get_args() );
+		}
+	}
+
+	/**
+	 * Alias; Add an internal method to an action hook if it hasn't already been.
+	 *
+	 * @since 2.0.0
+	 */
+	final public static function maybe_add_action() {
+		call_user_func_array( 'self::maybe_add_filter', func_get_args() );
 	}
 
 	/**
