9b7123426ee351ad2c140171cf0680507195c9a9
Doug Wollison <doug@wollison.net>
2015-11-12 11:58:04 -0500

    Added trailing returns

--- inc/compatibility-hooks.php
+++ inc/compatibility-hooks.php
@@ -14,3 +14,19 @@
 add_filter( 'option_blogname', 'nl_split_langs', 10, 1 );
 add_filter( 'option_blogdescription', 'nl_split_langs', 10, 1 );
 add_filter( 'the_title', 'nl_split_langs', 10, 1 );
+
+// =========================
+// ! Translation Aliases
+// =========================
+
+/**
+ * Alias of nl_get_translation but with current language.
+ *
+ * @see nl_get_translation()
+ */
+function nLingual_get_curlang_version( $id ) {
+	if ( !is_admin() ) {
+		$id = nL_get_translation( $id );
+	}
+	return $id;
+}
