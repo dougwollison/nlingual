3999195827a202947816c43cbb013b4035c55c4f
Doug Wollison <doug@wollison.net>
2015-11-16 14:33:10 -0500

    Added query filter for QuickStart's order manager.

--- inc/class-nlingual-liaison.php
+++ inc/class-nlingual-liaison.php
@@ -74,5 +74,44 @@ class Liaison extends Handler {
 			// Replace the retrieved index page's ID with it's translation counterpart
 			static::add_filter( 'qs_helper_get_index', 'current_language_post', 10, 1 );
 		}
+
+		// Order manager feature adjustments
+		if ( current_theme_supports( 'quickstart-order_manager' ) ) {
+			// Set language appropriately
+			static::maybe_add_filter( 'nlingual_pre_set_queried_language', 'quickstart_order_manager_language', 10, 2 );
+		}
+	}
+
+	/**
+	 * Set queried language to default (and un-assigned) for certain QuickStart queries.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param mixed    $pre_value The value to use instead of the determined one.
+	 * @param WP_Query $query     The query being modified.
+	 *
+	 * @return mixed The default language ot use.
+	 */
+	public static function quickstart_order_manager_language( $pre_value, \WP_Query $query ) {
+		// Get the context and the post type
+		$context = $query->get( 'qs-context' );
+		$post_type = $query->get( 'post_type' );
+
+		// If multiple post types, not the order manager context, or post type isn't supported, abort
+		if ( is_array( $post_type ) || $context != 'order-manager' || ! Registry::is_post_type_supported( $post_type ) ) {
+			return $pre_value;
+		}
+
+		// Get the sync rules for the post type
+		$rules = Registry::get_rules( 'sync', 'post_type', $post_type, 'post_fields' );
+		// If menu_order isn't set included in the sync rules, abort
+		if ( ! in_array( 'menu_order', $rules ) ) {
+			return $pre_value;
+		}
+
+		// Set the value to the default language (and no language)
+		$pre_value = array( Registry::default_language()->slug, '0' );
+
+		return $pre_value;
 	}
 }
