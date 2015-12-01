14ee8cc798de7dc7a9b5a0ad12ce235664a3bcd7
Doug Wollison <doug@wollison.net>
2015-12-01 15:50:20 -0500

    Remove link to old strings manager

--- doc/options/sidebar.php
+++ doc/options/sidebar.php
@@ -5,5 +5,4 @@
 	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-localizables' ); ?>" target="_blank"><?php _e( 'What should be localizable.' ); ?></a></li>
 	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-sync' ); ?>" target="_blank"><?php _e( 'Synchronizing and cloning options.' ); ?></a></li>
 	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-languages' ); ?>" target="_blank"><?php _e( 'What languages to use.' ); ?></a></li>
-	<li><a href="<?php echo admin_url( 'admin.php?page=nlingual-strings' ); ?>" target="_blank"><?php _e( 'Translate strings.' ); ?></a></li>
 </ul>
