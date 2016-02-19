eb3173d3c4cd4d1d58a37a9a57c0b0e6a0aa7f7c
Doug Wollison <doug@wollison.net>
2016-02-18 23:09:02 -0500

    Updated to use modified flag name

--- inc/class-nlingual-localizer.php
+++ inc/class-nlingual-localizer.php
@@ -1104,6 +1104,6 @@ class Localizer extends Handler {
 		}
 
 		// Not a valid method alias, throw exception
-		throw new Exception( sprintf( 'Call to unrecognized method alias %s::%s()', __CLASS__, $name ), NL_ERR_UNSUPPORTED_METHOD );
+		throw new Exception( sprintf( 'Call to unrecognized method alias %s::%s()', __CLASS__, $name ), NL_ERR_UNSUPPORTED );
 	}
 }
