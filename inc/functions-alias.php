238656f7af21586cd65f36ffb9b65c6d043c78af
Doug Wollison <doug@premise.ca>
2016-02-19 11:13:47 -0500

    Upgraded nl_get_option to use error handling

--- inc/functions-alias.php
+++ inc/functions-alias.php
@@ -12,13 +12,31 @@
  * @since 2.0.0
  */
 
+use nLingual\Exception  as Exception;
 use nLingual\Registry   as Registry;
 use nLingual\Translator as Translator;
 use nLingual\Rewriter   as Rewriter;
 
 /**
- * @see Registry::get() for details.
+ * Alias of Registry::get(), with error handling.
+ *
+ * @since 2.0.0
+ *
+ * @param string $option The name of the option to retrieve.
+ *
+ * @return mixed The value of that option (null if not found).
  */
 function nl_get_option( $option ) {
-	return Registry::get( $option );
+	// Attempt to retrieve the desired option.
+	try {
+		return Registry::get( $option );
+	} catch ( Exception $e ) {
+		// get_var and post_var have been merged into query_var
+		if ( $option == 'get_var' || $option == 'post_var' ) {
+			return nl_get_option( 'query_var' );
+		}
+	}
+
+	// Not found
+	return null;
 }
