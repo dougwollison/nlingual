8e962f3208c41df91cebae0bb1e56f0601717bb4
Doug Wollison <doug@wollison.net>
2016-02-21 15:48:32 -0500

    Moved sass file to css directory

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "scss"
+sass_dir = "css"
 images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "font"
