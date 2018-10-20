54d15ecd6271b39f1302226de6104d6ae6d88643
Doug Wollison <doug@wollison.net>
2018-10-20 13:05:19 -0400

    Move dashicon to placeholder

--- documentation/localizer/localize-this.php
+++ documentation/localizer/localize-this.php
@@ -1,5 +1,8 @@
 <title><?php _e( 'Localize This', 'nlingual' ); ?></title>
 
-<p><?php _e( 'Use the <i class="dashicons dashicons-translation"></i> widget to add, view, and edit localized versions of the text in the associated field.', 'nlingual' ); ?></p>
+<p><?php
+/* translators: %s = the translation icon HTML */
+_ef( 'Use the %s widget to add, view, and edit localized versions of the text in the associated field.', 'nlingual', '<i class="dashicons dashicons-translation"></i>' );
+?></p>
 
 <p><?php _e( 'When the site is viewed in a particular language and the data controlled by these fields is displayed, the version for that language will be used instead, provided it exists (it will fallback to the original value otherwise).', 'nlingual' ); ?></p>
