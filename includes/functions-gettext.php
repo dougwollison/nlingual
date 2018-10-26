1dedfb85ab6afa5380ccf24710aa21aeb80729c7
Doug Wollison <doug@wollison.net>
2018-10-26 11:00:44 -0400

    Fix up documentation of localized format utilities
    Describe uses, state return value

--- includes/functions-gettext.php
+++ includes/functions-gettext.php
@@ -18,12 +18,14 @@
  * @since 2.0.0 Reworked to accept separate args or and array of them.
  * @since 1.0.0
  *
- * @uses __()
+ * @uses __() to perform localizing.
  *
  * @param string $text   The format string.
  * @param string $domain The domain to use.
  * @param mixed  $args   The arguments for vsprintf().
  *                       This can be an array of arguments or separate ones.
+ *
+ * @return string The localized, completed string.
  */
 function _f( $text, $domain, $args ) {
 	// If $args isn't an array, get all addition arguments and use that
@@ -41,13 +43,15 @@ function _f( $text, $domain, $args ) {
  * @since 2.0.0 Reworked to accept separate args or and array of them.
  * @since 1.0.0
  *
- * @uses _x()
+ * @uses _x() to perform localizing.
  *
- * @param string $text The format string.
+ * @param string $text    The format string.
  * @param string $context The context to use.
- * @param string $domain The domain to use.
- * @param mixed  $args   The arguments for vsprintf().
- *                       This can be an array of arguments or separate ones.
+ * @param string $domain  The domain to use.
+ * @param mixed  $args    The arguments for vsprintf().
+ *                        This can be an array of arguments or separate ones.
+ *
+ * @return string The localized, completed string.
  */
 function _fx( $text, $context, $domain, $args ) {
 	// If $args isn't an array, get all addition arguments and use that
@@ -64,13 +68,9 @@ function _fx( $text, $context, $domain, $args ) {
  *
  * @since 1.0.0
  *
- * @see _f()
- *
- * @param string $text The format string.
- * @param string $domain The domain to use.
- * @params mixed $arg1.. The arguments for vsprintf().
+ * @see _f() for arguments.
  */
-function _ef( $text, $domain ) {
+function _ef( $text, $domain, $args ) {
 	echo call_user_func_array( '_f', func_get_args() );
 }
 
@@ -79,14 +79,9 @@ function _ef( $text, $domain ) {
  *
  * @since 1.0.0
  *
- * @see _fx()
- *
- * @param string $text The format string.
- * @param string $context The context to use.
- * @param string $domain The domain to use.
- * @params mixed $arg1.. The arguments for vsprintf().
+ * @see _fx() for arguments.
  */
-function _efx( $text, $context, $domain ) {
+function _efx( $text, $context, $domain, $args ) {
 	echo call_user_func_array( '_fx', func_get_args() );
 }
 
