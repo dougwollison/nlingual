13d479dfbe2e314d0d71796d52957789cb51309a
Doug Wollison <doug@premise.ca>
2019-08-06 16:50:56 -0400

    update version number and changelog

--- nlingual.php
+++ nlingual.php
@@ -3,7 +3,7 @@
 Plugin Name: nLingual
 Plugin URI: https://github.com/dougwollison/nlingual
 Description: Easy to manage Multilingual system, with theme development utilities and post data synchronization.
-Version: 2.8.8.1
+Version: 2.8.9
 Author: Doug Wollison
 Author URI: http://dougw.me
 Tags: multilingual, multi, language, admin, bilingual, switcher, translation, nlingual
@@ -50,7 +50,7 @@ define( 'NL_PLUGIN_SLUG', basename( NL_PLUGIN_DIR ) . '/' . basename( NL_PLUGIN_
  *
  * @var string
  */
-define( 'NL_PLUGIN_VERSION', '2.8.8' );
+define( 'NL_PLUGIN_VERSION', '2.8.9' );
 
 /**
  * Identifies the current database version.
