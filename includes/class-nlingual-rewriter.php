a51a5a29d78aa98b910a6e7881652e7445dd28bc
Doug Wollison <doug@premise.ca>
2020-03-09 17:07:41 -0400

    just use query var to get post type

--- includes/class-nlingual-rewriter.php
+++ includes/class-nlingual-rewriter.php
@@ -422,7 +422,7 @@ final class Rewriter {
 			}
 			// Post type archive? Get the link
 			elseif ( is_post_type_archive() ) {
-				$url = get_post_type_archive_link( get_queried_object()->name );
+				$url = get_post_type_archive_link( get_query_var( 'post_type' ) );
 			}
 			// Author archive? Get the link
 			elseif ( is_author() ) {
