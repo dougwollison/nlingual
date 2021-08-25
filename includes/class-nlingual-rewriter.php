33bd4439ad76799a6659cbb01159df5421ff8614
Doug Wollison <doug@wollison.net>
2021-08-25 11:38:28 -0400

    fix author URL rewriting
    apparently that function returns an html link, not the permalink like *every other _link() function*

--- includes/class-nlingual-rewriter.php
+++ includes/class-nlingual-rewriter.php
@@ -450,7 +450,7 @@ final class Rewriter {
 			}
 			// Author archive? Get the link
 			elseif ( is_author() ) {
-				$url = get_the_author_posts_link( get_queried_object_id() );
+				$url = get_author_posts_url( get_queried_object_id() );
 			}
 			// Date archive? Get link
 			elseif ( is_day() ) {
