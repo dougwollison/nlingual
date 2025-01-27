97b968b451a1f6829e9fd242c87da39e62f1169e
Doug Wollison <doug@wollison.net>
2025-01-27 13:08:31 -0500

    handle empty referer

--- includes/functions-nlingual.php
+++ includes/functions-nlingual.php
@@ -34,7 +34,7 @@ function is_backend() {
 
 	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 		// AJAX request, check if the referrer is from wp-admin
-		return strpos( $_SERVER['HTTP_REFERER'], admin_url() ) === 0;
+		return strpos( $_SERVER['HTTP_REFERER'] ?? '', admin_url() ) === 0;
 	}
 
 	// Check if in the admin or otherwise the login/register page
